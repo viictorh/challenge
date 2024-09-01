b2e5cb87d0774bf08ce53e02378a94b8cddc4f73
Victor Bello <victor.bello@voxage.com.br>
2024-09-01 12:23:02 -0300

    logbook funcionando.

--- src/main/java/com/hyperativa/challenge/logging/LogbookConfig.java
+++ src/main/java/com/hyperativa/challenge/logging/LogbookConfig.java
@@ -1,42 +1,25 @@
 package com.hyperativa.challenge.logging;
 
-import java.util.Set;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.core.Conditions;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
+import org.zalando.logbook.core.DefaultSink;
 
 @Configuration
 public class LogbookConfig {
 
-	 @Bean  @ConditionalOnBean(Logbook.class)
-	    public Logbook logbook() {	 
-		 
-	        Logbook logbook = Logbook.builder()
-	            .condition(Conditions.exclude(
-							Conditions.requestTo("/health"),
-							Conditions.requestTo("/admin/**"),
-							Conditions.contentType("application/octet-stream"),
-							Conditions.header("X-Secret", Set.of("1", "true")::contains)))
-					.build();
-	        return logbook;
-	    }
-//	@Bean
-//	public BodyFilter bodyFilter() {
-//	    return HeaderFilter.merge(
-//	    		BodyFilters.defaultValue(), 
-//	    		HeaderFilter.repl(Collections.singleton("secret"), "XXX"));
-//	}
-//	
-//    @Bean
-//    public HeaderFilter authorizationHeaderFilter() {
-//        return HeaderFilter.replaceHeaders("Authorization", "<filtered>");
-//    }
-//
-//    @Bean
-//    public QueryFilter queryFilter() {
-//        return QueryFilter.replaceQuery("password", "<filtered>");
-//    }
+	@Bean
+	@ConditionalOnBean(Logbook.class)
+	public Logbook logbook() {
+		Logbook logbook = Logbook.builder()
+				.condition(Conditions.exclude(Conditions.requestTo("/api/welcome"),
+						Conditions.contentType("application/octet-stream"), Conditions.header("Authorization", "true")))
+				.sink(new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter())).build();
+		return logbook;
+	}
+
 }
